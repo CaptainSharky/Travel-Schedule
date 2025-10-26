8e4efd718f0b9a7fdfafe3108c42170b231ee019
CaptainSharky <chuikostepan@mail.ru>
2025-10-26 15:36:04 +0300

    build(TrainsApp): инициализирован проект Xcode

---
+++ TrainsApp/TrainsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrainsApp
+//
+//  Created by Stepan Chuiko on 26.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrainsApp/TrainsApp/TrainsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsAppApp.swift
+//  TrainsApp
+//
+//  Created by Stepan Chuiko on 26.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrainsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
