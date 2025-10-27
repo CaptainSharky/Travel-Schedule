819662ec8e4af45733c180d7fe002550507edd42
CaptainSharky <chuikostepan@mail.ru>
2025-10-27 13:52:38 +0300

    feat(Services): CopyrightService

--- TrainsApp/TrainsApp/ContentView.swift
+++ TrainsApp/TrainsApp/ContentView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import OpenAPIURLSession
 
 struct ContentView: View {
+    let apikey = "bb6ebbc8-d4ec-44d1-a927-87d67ced6345"
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +19,59 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            testFetchCopyright()
+        }
+    }
+
+    func testFetchStations() {
+        Task {
+            do {
+                let client = Client(
+                    serverURL: try Servers.Server1.url(),
+                    transport: URLSessionTransport()
+                )
+
+                let service = NearestStationsService(
+                    client: client,
+                    apikey: apikey
+                )
+
+                print("Fetching stations...")
+                let stations = try await service.getNearestStations(
+                    lat: 59.864177,
+                    lng: 30.319163,
+                    distance: 50
+                )
+
+                print("Successfully fetched stations: \(stations)")
+            } catch {
+                print("Error fetching stations: \(error)")
+            }
+        }
+    }
+
+    func testFetchCopyright() {
+        Task {
+            do {
+                let client = Client(
+                    serverURL: try Servers.Server1.url(),
+                    transport: URLSessionTransport()
+                )
+
+                let service = CopyrightService(
+                    client: client,
+                    apikey: apikey
+                )
+
+                print("Fetching copyright...")
+                let copyright = try await service.getCopyright()
+
+                print("Successfully fetched copyright: \(copyright)")
+            } catch {
+                print("Error fetching copyright: \(error)")
+            }
+        }
     }
 }
 
