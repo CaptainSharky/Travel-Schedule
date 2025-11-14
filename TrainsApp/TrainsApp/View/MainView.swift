30c06a7544cc53b052fd031843e1b9d763ce8bbc
CaptainSharky <chuikostepan@mail.ru>
2025-11-14 23:43:46 +0300

    feat(Assets): добавлены цветы приложения

---
+++ TrainsApp/TrainsApp/View/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  TrainsApp
+//
+//  Created by Stepan Chuiko on 14.11.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
